765c4ad47801f9d1a7384f65cb4bc7787838d83a
Michael Conner <michaelconner@hotmail.co.uk>
2018-11-26 16:26:25 +0000

    section B parts 4-5 completed

--- sports_team/specs/sports_team_spec.rb
+++ sports_team/specs/sports_team_spec.rb
@@ -7,27 +7,59 @@ class TestSportsTeam < MiniTest::Test
 
 def test_get_team_name
   team = SportsTeam.new('CodeClan Utd',['Mark'],'Michael')
-  result = team.get_team_name
+  result = team.team_name
   assert_equal('CodeClan Utd', result)
 end
 
 def test_get_players
   team = SportsTeam.new('CodeClan Utd',['Mark','Lionel Messi'],'Michael')
-  result = team.get_players
+  result = team.players
   assert_equal(['Mark','Lionel Messi'], result)
 end
 
 def test_get_coach
   team = SportsTeam.new('CodeClan Utd',['Mark'],'Michael')
-  result = team.get_coach
+  result = team.coach
   assert_equal('Michael', result)
 end
 
 def test_set_coach
   team = SportsTeam.new('CodeClan Utd',['Mark'],'Michael')
-  result = team.set_coach('Carlo Ancelotti')
+  result = team.coach='Carlo Ancelotti'
   assert_equal('Carlo Ancelotti', result)
 end
+
+def test_add_player__Roberto_Baggio
+  team = SportsTeam.new('CodeClan Utd',['Mark', 'Lionel Messi'],'Michael')
+  team.add_player('Roberto Baggio')
+  result = team.players.include?('Roberto Baggio')
+  assert_equal(result, true)
+end
+
+def test_add_player__Roberto_Baggio
+  team = SportsTeam.new('CodeClan Utd',['Mark', 'Lionel Messi'],'Michael')
+  team.add_player('Roberto Baggio')
+  result = team.players.include?('Roberto Baggio')
+  assert_equal(result, true)
+end
+
+def test_find_player__Lionel_Messi__True
+  team = SportsTeam.new('CodeClan Utd',['Mark', 'Lionel Messi'],'Michael')
+  result = team.find_player('Lionel Messi')
+  assert_equal(result, true)
+end
+
+def test_find_player__Fred_Flintstone__False
+  team = SportsTeam.new('CodeClan Utd',['Mark', 'Lionel Messi'],'Michael')
+  result = team.find_player('Fred Flintstone')
+  assert_equal(result, false)
+end
+
+
+
+
+
+
 
 
 end
--- sports_team/sports_team.rb
+++ sports_team/sports_team.rb
@@ -1,6 +1,7 @@
 class SportsTeam
 
-
+attr_reader :team_name, :players
+attr_accessor :coach
 
 def initialize(team_name, players, coach)
 @team_name = team_name
@@ -9,21 +10,33 @@ def initialize(team_name, players, coach)
 end
 
 
-def get_team_name
-  return @team_name
+# def get_team_name
+#   return @team_name
+# end
+#
+# def get_players
+#   return @players
+# end
+#
+# def get_coach
+#   return @coach
+# end
+#
+# def set_coach(coach)
+#   @coach = coach
+# end
+
+def add_player(player)
+  @players << player
 end
 
-def get_players
-  return @players
+def find_player(player)
+  @players.include?(player)
 end
 
-def get_coach
-  return @coach
-end
 
-def set_coach(coach)
-  @coach = coach
-end
+
+
 
 
 
