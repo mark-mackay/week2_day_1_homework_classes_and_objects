434e76fbc5f701fc5e4955119f962c97357bf6cb
Mark Mackay <Mark_Mackay@yahoo.com>
2018-11-26 20:46:39 +0000

    Did optional extra's - still to document

--- library/library.rb
+++ library/library.rb
@@ -1,5 +1,5 @@
 class Library
-
+@new_book = {:title=>"1984", :rental_details=>{:student_name=>"Mark", :date=>"23/04/18"}}
 def initialize(books)
   @books = books
 end
@@ -7,6 +7,30 @@ end
 def get_books
   return @books
 end
+
+def get_book_by_title(title)
+ match = nil
+  for book in @books
+    match = book if book[:title] == title
+  end
+  return match
+end
+
+def return_rental_details(title)
+  book = get_book_by_title(title)
+  return book[:rental_details]
+end
+
+def add_new_book(title)
+  new_book = {:title=>title, :rental_details=>{:student_name=>"", :date=>""}}
+  @books << new_book
+end
+
+def change_rental_details(title, name, date)
+  @books.each { |book| book[:rental_details] = {:student_name => name, :date => date } if book[:title] == title }
+end
+
+
 
 
 
--- library/specs/library_specs.rb
+++ library/specs/library_specs.rb
@@ -50,18 +50,37 @@ class TestLibrary < MiniTest::Test
            date: "22/08/18"
           }
   }]
+  @expected_book = {:title=>"1984", :rental_details=>{:student_name=>"Mark", :date=>"23/04/18"}}
+  @expected_details = {:student_name=>"Alice", :date=>"22/08/18"}
 end
 
   def test_get_books
     books_output = Library.new(@books)
-    p books_output
   end
 
+  def test_get_book_by_title__1984
+    books_output = Library.new(@books)
+    result = books_output.get_book_by_title("1984")
+    assert_equal(@expected_book, result)
+  end
 
+  def test_return_rental_details__brave__new__world
+    books_output = Library.new(@books)
+    result = books_output.return_rental_details("brave_new_world")
+    assert_equal(@expected_details, result)
+  end
 
+  def test_add_new_book
+    books_output = Library.new(@books)
+    books_output.add_new_book("where's_wally?")
+    result = books_output.get_book_by_title("where's_wally?")
+    assert_equal(result[:title], "where's_wally?")
+  end
 
-
-
-
-
+  def test_change_rental_details
+    books_output = Library.new(@books)
+    books_output.change_rental_details("catcher_in_the_rye", "Neville", "26/11/2018")
+    result = books_output.get_book_by_title("catcher_in_the_rye")
+    assert_equal(result[:rental_details], {:student_name=>"Neville", :date=>"26/11/2018"})
+  end
 end
